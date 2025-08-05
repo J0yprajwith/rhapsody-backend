837199ef4ad42ca419416d5064019033389a0655
JOYPRAJWITH KALLEPALLY <joyprajwith1209@gmail.com>
2025-08-05 18:40:33 +0530

    made changes in packages

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ["https://shopify-frontend-two.vercel.app", "http://localhost:5173", "https://shopify-frontend-git-main-sekhars-projects-b5d3087c.vercel.app", "https://shopify-frontend-three.vercel.app"],
+    origin: [process.env.FRONTEND_URL || "http://localhost:4173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
